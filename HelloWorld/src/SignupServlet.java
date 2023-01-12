805e07b3d5197da709e0fe79f896c99d851caa1b
dawnpoem <dawnpoem@naver.com>
2023-01-12 17:22:52 +0900

    230112 servlet
    서블릿 실습

---
+++ HelloWorld/src/SignupServlet.java
@@ -0,0 +1,51 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SignupServlet
+ */
+@WebServlet("/SignupServlet")
+public class SignupServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SignupServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.append("<!doctype html><html><head><title>Hello World Servlet</title></head><body>")
+		.append("UserID : " + request.getParameter("id"))
+		.append("<br>Password : " + request.getParameter("ps"))
+		.append("<br>Name : " + request.getParameter("name"))
+		.append("</body></html>");;
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
